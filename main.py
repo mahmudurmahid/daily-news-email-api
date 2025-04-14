f45883fa6d517d2d3d102e3c10a6256729a3e589
mahmudurmahid <m.mahid@icloud.com>
2025-04-14 17:04:48 +0100

    finalized email function and app

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import os
+from sendemail import send_email
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,6 +11,11 @@ url = f"https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey={api_key}
 request = requests.get(url)
 content = request.json()
 
+news = ""
+
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    if article["title"] is not None:
+        news = news + article["title"].upper() + "\n" + str(article["description"]) + 2*"\n"
+
+news = news.encode("utf-8")
+send_email(message=news)
---
+++ sendemail.py
@@ -0,0 +1,17 @@
+import smtplib, ssl
+import os
+from dotenv import load_dotenv
+
+def send_email(message):
+    host = os.getenv("HOST")
+    port = os.getenv("PORT")
+
+    username = os.getenv("USERNAME")
+    password = os.getenv("PASSWORD")
+
+    receiver = os.getenv("RECEIVER")
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
