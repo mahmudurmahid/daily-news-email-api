8a82e7762e1028c8e5749e2bb38b8212af7b16c9
mahmudurmahid <m.mahid@icloud.com>
2025-04-14 15:30:46 +0100

    added url and requests

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv("API_KEY")
+url = f"https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey={api_key}"
+
+request = requests.get(url)
+content = request.json()
+
+for article in content["articles"]:
+    print(article["title"])
+    print(article["description"])
