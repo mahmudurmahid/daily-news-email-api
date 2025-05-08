f936544be759c8c16343d163055634a6e1215dfb
mahmudurmahid <m.mahid@icloud.com>
2025-05-08 18:47:24 +0100

    refactored code with comments, no functional change

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ load_dotenv()
 
 source = "techcrunch"
 
+# Requirements for api
 api_key = os.getenv("API_KEY")
 url = f"https://newsapi.org/v2/top-headlines?sources={source}&apiKey={api_key}&language=en"
 
 request = requests.get(url)
 content = request.json()
 
+# Format the top 20 news articles into an email-friendly string and send it
 news = "Subject: Today's Tech News\n\n"
 
 for article in content["articles"][:20]:
@@ -20,4 +22,4 @@ for article in content["articles"][:20]:
         news = news + article["title"].upper() + "\n" + str(article["description"]) + "\n" + article["url"] + 2*"\n"
 
 news = news.encode("utf-8")
-send_email(message=news)
+send_email(message=news) # use email function from sendemail.py
